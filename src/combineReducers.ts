fb193c5597af9bd36e1fd84694e79b9fab2f7753
Simon Green <simon@captaincodeman.com>
2019-12-01 12:36:13 -0700

    Make persist options optional Add some impl notes Remove redux-devtools-extension

--- src/combineReducers.ts
+++ src/combineReducers.ts
@@ -1,6 +1,8 @@
 import { Action, Reducer, Reducers, State } from '../typings'
 
 export function combineReducers<R extends Reducers>(reducers: R): Reducer<State<R>> {
+  // NOTE: we don't re-create the root state object each time, that stays as it is
+  // this allows us to just pass the reference instead of needing the getState() fn
   return (state: State<R> = {} as State<R>, action: Action) => {
     for (const key in reducers) {
       state[key] = reducers[key](state[key], action)
--- src/persist.ts
+++ src/persist.ts
@@ -5,17 +5,21 @@ export interface Options<S> {
   // name sets the state key to use, useful in development to avoid conflict
   // with other apps. Default is to use the app location hostname
   name: string
+
   // filter predicate allows control over whether to persist state based on 
   // the action. Default is to trigger persistence after all actions
   filter: (action: Action) => boolean
+
   // persist allows transforming the state to only persist part of it.
   // Default is to persist complete state
   persist: (state: S) => Partial<S>
+
   // delay introduces a delay before the save is performed. If another persist
   // is triggered before it expires, the previous persist is cancelled and a
   // new one scheduled. This can save doing too many persist operations by
   // debouncing the triggering. Default is 0 delay
   delay: number
+
   // TODO: version for updates, expiry etc...
 }
 
@@ -24,7 +28,7 @@ export interface Options<S> {
 //       or provide hooks to transform (before save / after load)
 // TODO: provide methods to purge, clear, flush etc... (state / actions?)
 
-export function persist<S>(store: Store<S>, options: Partial<Options<S>>) {
+export function persist<S>(store: Store<S>, options?: Partial<Options<S>>) {
   const opt = {
     name: location.hostname,
     filter: (_action: Action) => true,
